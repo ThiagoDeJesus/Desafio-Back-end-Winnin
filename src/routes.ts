d80db0cb6cc839522eff497b74749ef34bfe6a09
ThiagoDeJesus <99thiagodejesus@gmail.com>
2021-11-17 20:56:33 -0300

    feat: add redirect to docs on home page

--- src/routes.ts
+++ src/routes.ts
@@ -5,7 +5,7 @@ import { PostControllerFactory } from "./modules/Post/CreatePostFactory"
 const router = Router()
 
 router.get("/", (req, res) => {
-  res.json({ message: "Hello world!" })
+  res.redirect("/api-docs")
 })
 
 router.get("/posts", (req, res) => PostControllerFactory().getPosts(req, res))
